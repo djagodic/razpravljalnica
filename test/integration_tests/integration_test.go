aea3918ab765805ecc351e1836e105adf91cd9e1
Jaka Furlan <jaka.furlan@cosylab.com>
2026-01-02 17:55:47 +0100

    dodal se head in tail odpoved integration test

--- test/integration_tests/integration_test.go
+++ test/integration_tests/integration_test.go
@@ -3,6 +3,7 @@ package integration_test
 import (
 	"context"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"os/exec"
@@ -14,6 +15,7 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 
+	nadzorna_ravnina "github.com/djagodic/razpravljalnica/pkg/api/nadzornaRavnina"
 	razpravljalnica "github.com/djagodic/razpravljalnica/pkg/api/razpravljalnica"
 
 	"github.com/stretchr/testify/require"
@@ -405,6 +407,381 @@ func TestIntegrationReplicationWithMiddleNodeFailure(t *testing.T) {
 		stopProcess(midCmd)
 		time.Sleep(4 * time.Second) //pocakamo 3 s da control zazna da je vozlisce mrtvo
 
+		msgResp, err = headClient.PostMessage(ctx, &razpravljalnica.PostMessageRequest{
+			TopicId: topicResp.Id,
+			UserId:  u.Id,
+			Text:    "What about the 2nd breakfast?",
+		})
+		require.NoError(t, err)
+		require.NotNil(t, msgResp)
+
+		require.Eventually(t, func() bool {
+			messages, err := tailClient.GetMessages(ctx, &razpravljalnica.GetMessagesRequest{
+				TopicId: topicResp.Id,
+			})
+			if err != nil {
+				fmt.Print(err)
+				return false
+			}
+			for _, m := range messages.Messages {
+				if m.Id == msgResp.Id && m.Text == msgResp.Text && m.Likes == 0 {
+					return true
+				}
+			}
+			return false
+		}, 5*time.Second, 50*time.Millisecond)
+	})
+
+}
+
+// pomozna funkcija iz cmd\server\main
+func getClusterState(controlAddr string) (*nadzorna_ravnina.NodeInfo, *nadzorna_ravnina.NodeInfo, error) {
+	conn, err := grpc.NewClient(controlAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, nil, err
+	}
+	defer conn.Close()
+	ctrlClient := nadzorna_ravnina.NewControlPlaneClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := ctrlClient.GetClusterState(ctx, &emptypb.Empty{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return resp.Head, resp.Tail, nil
+}
+
+// pomozna funkcija iz cmd\server\main
+func connectToNode(address string) (razpravljalnica.MessageBoardClient, *grpc.ClientConn) {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to node %s: %v", address, err)
+	}
+	return razpravljalnica.NewMessageBoardClient(conn), conn
+}
+
+// test kjer preverimo delovanje pri odpovedi glave. preden posljemo drugo sporocilo (po spremembi stanja vozlisc se vprasamo controlno ravnino za novo glavo)
+func TestIntegrationReplicationWithHeadNodeFailure(t *testing.T) {
+	ctx := context.Background()
+
+	// Assign ports
+	ctrlAddr, err := getFreePort()
+	require.NoError(t, err)
+
+	headAddr, err := getFreePort()
+	require.NoError(t, err)
+
+	midAddr, err := getFreePort()
+	require.NoError(t, err)
+
+	tailAddr, err := getFreePort()
+	require.NoError(t, err)
+
+	//naredimo binarne datoteke
+	buildBinary(t, "../../cmd/control", "./control.exe")
+	buildBinary(t, "../../cmd/server", "./server.exe")
+
+	// Clean up binaries after test
+	defer func() {
+		os.Remove("./control.exe")
+		os.Remove("./server.exe")
+	}()
+
+	// --- 1. Start control plane process ---
+	ctrlCmd := startProcess(t, "./control.exe", []string{"-addr=" + ctrlAddr})
+	defer stopProcess(ctrlCmd)
+
+	// Wait for control plane to be ready
+	require.NoError(t, waitForPort(ctrlAddr, 2*time.Second))
+	time.Sleep(50 * time.Millisecond) // allow registration
+
+	// --- 2. Start head and mid and tail servers ---
+	headCmd := startProcess(t, "./server.exe", []string{
+		"-id=head-1",
+		"-addr=" + headAddr,
+		"-addrControl=" + ctrlAddr,
+	})
+	defer stopProcess(headCmd)
+	time.Sleep(50 * time.Millisecond) // allow registration
+
+	midCmd := startProcess(t, "./server.exe", []string{
+		"-id=mid-2",
+		"-addr=" + midAddr,
+		"-addrControl=" + ctrlAddr,
+	})
+	defer stopProcess(midCmd)
+	time.Sleep(50 * time.Millisecond) // allow registration
+
+	tailCmd := startProcess(t, "./server.exe", []string{
+		"-id=tail-3",
+		"-addr=" + tailAddr,
+		"-addrControl=" + ctrlAddr,
+	})
+	defer stopProcess(tailCmd)
+	time.Sleep(50 * time.Millisecond) // allow registration
+
+	// Wait for servers to be ready
+	require.NoError(t, waitForPort(headAddr, 2*time.Second))
+	require.NoError(t, waitForPort(tailAddr, 2*time.Second))
+
+	// --- 3. Connect clients to head and tail ---
+	headConn := connectGRPC(t, headAddr)
+	defer headConn.Close()
+	tailConn := connectGRPC(t, tailAddr)
+	defer tailConn.Close()
+
+	headClient := razpravljalnica.NewMessageBoardClient(headConn)
+	//head client mora dejansko biti registriran kot user, ker bo pisal:
+	u, err := headClient.CreateUser(
+		context.Background(),
+		&razpravljalnica.CreateUserRequest{Name: "Frodo"},
+	)
+	if err != nil {
+		fmt.Println("Error creating user:", err)
+
+	}
+	tailClient := razpravljalnica.NewMessageBoardClient(tailConn)
+
+	//naredimo topic in postamo message
+	//spremenljivke da imamo shranjen topic in msg s katerim testiramo
+	var topicResp *razpravljalnica.Topic
+	var msgResp *razpravljalnica.Message
+	t.Run("CreateTopic", func(t *testing.T) {
+		topicResp, err = headClient.CreateTopic(ctx, &razpravljalnica.CreateTopicRequest{
+			Name: "LOTR debate",
+		})
+		require.NoError(t, err)
+		require.NotNil(t, topicResp)
+
+		// Wait for replication
+		require.Eventually(t, func() bool {
+			resp, err := tailClient.ListTopics(ctx, &emptypb.Empty{})
+			if err != nil {
+				return false
+			}
+			for _, tpc := range resp.Topics {
+				if tpc.Id != topicResp.Id {
+					return false
+				}
+			}
+			return true
+		}, 5*time.Second, 50*time.Millisecond)
+	})
+
+	t.Run("PostMessage", func(t *testing.T) {
+		msgResp, err = headClient.PostMessage(ctx, &razpravljalnica.PostMessageRequest{
+			TopicId: topicResp.Id,
+			UserId:  u.Id,
+			Text:    "You shall not pass!",
+		})
+		require.NoError(t, err)
+		require.NotNil(t, msgResp)
+
+		require.Eventually(t, func() bool {
+			messages, err := tailClient.GetMessages(ctx, &razpravljalnica.GetMessagesRequest{
+				TopicId: topicResp.Id,
+			})
+			if err != nil {
+				return false
+			}
+			for _, m := range messages.Messages {
+				if m.Id != msgResp.Id || m.Text != msgResp.Text || m.Likes != 0 {
+					return false
+				}
+			}
+			return true
+		}, 5*time.Second, 50*time.Millisecond)
+	})
+
+	t.Run("PostMessageAfterReconfigure", func(t *testing.T) {
+		//ubijemo glavo
+		stopProcess(headCmd)
+		time.Sleep(4 * time.Second) //pocakamo 3 s da control zazna da je vozlisce mrtvo
+
+		//dobimo novo glavo
+		head, _, err := getClusterState(ctrlAddr)
+		if err != nil {
+			log.Fatalf("Failed to get cluster state: %v", err)
+		}
+		headClient, connHead := connectToNode(head.Address)
+		defer connHead.Close()
+
+		//posljemo sporocilo v novo glavo
+		msgResp, err = headClient.PostMessage(ctx, &razpravljalnica.PostMessageRequest{
+			TopicId: topicResp.Id,
+			UserId:  u.Id,
+			Text:    "What about the 2nd breakfast?",
+		})
+		require.NoError(t, err)
+		require.NotNil(t, msgResp)
+
+		require.Eventually(t, func() bool {
+			messages, err := tailClient.GetMessages(ctx, &razpravljalnica.GetMessagesRequest{
+				TopicId: topicResp.Id,
+			})
+			if err != nil {
+				fmt.Print(err)
+				return false
+			}
+			for _, m := range messages.Messages {
+				if m.Id == msgResp.Id && m.Text == msgResp.Text && m.Likes == 0 {
+					return true
+				}
+			}
+			return false
+		}, 5*time.Second, 50*time.Millisecond)
+	})
+
+}
+
+func TestIntegrationReplicationWithTailNodeFailure(t *testing.T) {
+	ctx := context.Background()
+
+	// Assign ports
+	ctrlAddr, err := getFreePort()
+	require.NoError(t, err)
+
+	headAddr, err := getFreePort()
+	require.NoError(t, err)
+
+	midAddr, err := getFreePort()
+	require.NoError(t, err)
+
+	tailAddr, err := getFreePort()
+	require.NoError(t, err)
+
+	//naredimo binarne datoteke
+	buildBinary(t, "../../cmd/control", "./control.exe")
+	buildBinary(t, "../../cmd/server", "./server.exe")
+
+	// Clean up binaries after test
+	defer func() {
+		os.Remove("./control.exe")
+		os.Remove("./server.exe")
+	}()
+
+	// --- 1. Start control plane process ---
+	ctrlCmd := startProcess(t, "./control.exe", []string{"-addr=" + ctrlAddr})
+	defer stopProcess(ctrlCmd)
+
+	// Wait for control plane to be ready
+	require.NoError(t, waitForPort(ctrlAddr, 2*time.Second))
+	time.Sleep(50 * time.Millisecond) // allow registration
+
+	// --- 2. Start head and mid and tail servers ---
+	headCmd := startProcess(t, "./server.exe", []string{
+		"-id=head-1",
+		"-addr=" + headAddr,
+		"-addrControl=" + ctrlAddr,
+	})
+	defer stopProcess(headCmd)
+	time.Sleep(50 * time.Millisecond) // allow registration
+
+	midCmd := startProcess(t, "./server.exe", []string{
+		"-id=mid-2",
+		"-addr=" + midAddr,
+		"-addrControl=" + ctrlAddr,
+	})
+	defer stopProcess(midCmd)
+	time.Sleep(50 * time.Millisecond) // allow registration
+
+	tailCmd := startProcess(t, "./server.exe", []string{
+		"-id=tail-3",
+		"-addr=" + tailAddr,
+		"-addrControl=" + ctrlAddr,
+	})
+	defer stopProcess(tailCmd)
+	time.Sleep(50 * time.Millisecond) // allow registration
+
+	// Wait for servers to be ready
+	require.NoError(t, waitForPort(headAddr, 2*time.Second))
+	require.NoError(t, waitForPort(tailAddr, 2*time.Second))
+
+	// --- 3. Connect clients to head and tail ---
+	headConn := connectGRPC(t, headAddr)
+	defer headConn.Close()
+	tailConn := connectGRPC(t, tailAddr)
+	defer tailConn.Close()
+
+	headClient := razpravljalnica.NewMessageBoardClient(headConn)
+	//head client mora dejansko biti registriran kot user, ker bo pisal:
+	u, err := headClient.CreateUser(
+		context.Background(),
+		&razpravljalnica.CreateUserRequest{Name: "Frodo"},
+	)
+	if err != nil {
+		fmt.Println("Error creating user:", err)
+
+	}
+	tailClient := razpravljalnica.NewMessageBoardClient(tailConn)
+
+	//naredimo topic in postamo message
+	//spremenljivke da imamo shranjen topic in msg s katerim testiramo
+	var topicResp *razpravljalnica.Topic
+	var msgResp *razpravljalnica.Message
+	t.Run("CreateTopic", func(t *testing.T) {
+		topicResp, err = headClient.CreateTopic(ctx, &razpravljalnica.CreateTopicRequest{
+			Name: "LOTR debate",
+		})
+		require.NoError(t, err)
+		require.NotNil(t, topicResp)
+
+		// Wait for replication
+		require.Eventually(t, func() bool {
+			resp, err := tailClient.ListTopics(ctx, &emptypb.Empty{})
+			if err != nil {
+				return false
+			}
+			for _, tpc := range resp.Topics {
+				if tpc.Id != topicResp.Id {
+					return false
+				}
+			}
+			return true
+		}, 5*time.Second, 50*time.Millisecond)
+	})
+
+	t.Run("PostMessage", func(t *testing.T) {
+		msgResp, err = headClient.PostMessage(ctx, &razpravljalnica.PostMessageRequest{
+			TopicId: topicResp.Id,
+			UserId:  u.Id,
+			Text:    "You shall not pass!",
+		})
+		require.NoError(t, err)
+		require.NotNil(t, msgResp)
+
+		require.Eventually(t, func() bool {
+			messages, err := tailClient.GetMessages(ctx, &razpravljalnica.GetMessagesRequest{
+				TopicId: topicResp.Id,
+			})
+			if err != nil {
+				return false
+			}
+			for _, m := range messages.Messages {
+				if m.Id != msgResp.Id || m.Text != msgResp.Text || m.Likes != 0 {
+					return false
+				}
+			}
+			return true
+		}, 5*time.Second, 50*time.Millisecond)
+	})
+
+	t.Run("PostMessageAfterReconfigure", func(t *testing.T) {
+		//ubijemo tail
+		stopProcess(tailCmd)
+		time.Sleep(4 * time.Second) //pocakamo 3 s da control zazna da je vozlisce mrtvo
+
+		//dobimo nov rep
+		_, tail, err := getClusterState(ctrlAddr)
+		if err != nil {
+			log.Fatalf("Failed to get cluster state: %v", err)
+		}
+		tailClient, connTail := connectToNode(tail.Address)
+		defer connTail.Close()
+
+		//posljemo sporocilo v glavo
 		msgResp, err = headClient.PostMessage(ctx, &razpravljalnica.PostMessageRequest{
 			TopicId: topicResp.Id,
 			UserId:  u.Id,
